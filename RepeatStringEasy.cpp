c96625ac7c230a414f3159df65d1a5ec9d57f87a
Sarthak Shah <Sarthakshah30@rocketmail.com>
2017-03-14 18:12:14 +0530

    Add files via upload

---
+++ RepeatStringEasy.cpp
@@ -0,0 +1,203 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class RepeatStringEasy {
+public:
+	string str1,str2;
+	int maximalLength(string);
+	int dp[50][50];
+	int calc(int i,int j);
+};
+
+int RepeatStringEasy::maximalLength(string s) {
+	int max=0;
+	for(int i = 0 ; i < s.size()-1 ; i++){
+		str1 = s.substr(0,i+1);
+		str2 = s.substr(i+1,s.size()-i-1);
+		memset(dp,-1,sizeof(dp));
+		int local = calc(i,s.size()-i-2);
+		if(local > max)
+		max=local;
+	}
+	return (max*2);
+}
+
+int RepeatStringEasy::calc(int i,int j){
+	if(i<0 || j<0)
+	return 0;
+	int local=0;
+	if(dp[i][j]!=-1)
+	return dp[i][j];
+	else if(str1[i]==str2[j]){
+		local = 1+calc(i-1,j-1);
+	}
+	else if(str1[i]!=str2[j]){
+		local = max(calc(i-1,j),calc(i,j-1));
+	}
+	dp[i][j]=local;
+	return local;
+}
+
+
+double test0() {
+	string p0 = "frankfurt";
+	RepeatStringEasy * obj = new RepeatStringEasy();
+	clock_t start = clock();
+	int my_answer = obj->maximalLength(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "single";
+	RepeatStringEasy * obj = new RepeatStringEasy();
+	clock_t start = clock();
+	int my_answer = obj->maximalLength(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "singing";
+	RepeatStringEasy * obj = new RepeatStringEasy();
+	clock_t start = clock();
+	int my_answer = obj->maximalLength(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 6;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "aababbababbabbbbabbabb";
+	RepeatStringEasy * obj = new RepeatStringEasy();
+	clock_t start = clock();
+	int my_answer = obj->maximalLength(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 18;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "x";
+	RepeatStringEasy * obj = new RepeatStringEasy();
+	clock_t start = clock();
+	int my_answer = obj->maximalLength(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
