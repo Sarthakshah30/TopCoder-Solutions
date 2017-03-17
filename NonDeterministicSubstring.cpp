97733073d59aceb85bed74e39bdb12291b450a3e
Sarthak Shah <Sarthakshah30@rocketmail.com>
2017-03-17 23:07:42 +0530

    Add files via upload

---
+++ NonDeterministicSubstring.cpp
@@ -0,0 +1,202 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class NonDeterministicSubstring {
+public:
+	long long ways(string, string);
+};
+
+long long NonDeterministicSubstring::ways(string A, string B) {
+	vector<string> strings;
+	if(A.size()<B.size())
+	return 0;
+	
+	for(int i = 0 ; i <=A.size()-B.size() ; i++){
+			string str=A.substr(i,B.size());
+			vector<string>::iterator it;
+			it = std::find(strings.begin(),strings.end(),str);
+			if(it==strings.end())
+			strings.push_back(A.substr(i,B.size()));
+	}
+	long long int count=0;
+	for(int i = 0 ; i<strings.size() ; i++){
+		int j=0;
+		for( ; j < B.size() ; j++){
+			if(strings[i][j]==B[j])
+			continue;
+			if(B[j]=='?')
+			continue;
+			break;
+		}
+		if(j==B.size())
+		count++;
+	}
+	return count;
+}
+
+
+double test0() {
+	string p0 = "00010001";
+	string p1 = "??";
+	NonDeterministicSubstring * obj = new NonDeterministicSubstring();
+	clock_t start = clock();
+	long long my_answer = obj->ways(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	long long p2 = 3LL;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "00000000";
+	string p1 = "??0??0";
+	NonDeterministicSubstring * obj = new NonDeterministicSubstring();
+	clock_t start = clock();
+	long long my_answer = obj->ways(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	long long p2 = 1LL;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "001010101100010111010";
+	string p1 = "???";
+	NonDeterministicSubstring * obj = new NonDeterministicSubstring();
+	clock_t start = clock();
+	long long my_answer = obj->ways(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	long long p2 = 8LL;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "00101";
+	string p1 = "??????";
+	NonDeterministicSubstring * obj = new NonDeterministicSubstring();
+	clock_t start = clock();
+	long long my_answer = obj->ways(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	long long p2 = 0LL;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	string p0 = "1101010101111010101011111111110001010101";
+	string p1 = "???????????????????????????????????";
+	NonDeterministicSubstring * obj = new NonDeterministicSubstring();
+	clock_t start = clock();
+	long long my_answer = obj->ways(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	long long p2 = 6LL;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
