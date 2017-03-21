d93e5d299d1b5b97069e9f9bbb32a592b4cada82
Sarthak Shah <Sarthakshah30@rocketmail.com>
2017-03-22 00:30:51 +0530

    Add files via upload

---
+++ ExactTreeEasy.cpp
@@ -0,0 +1,290 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class ExactTreeEasy {
+public:
+	vector <int> getTree(int, int);
+};
+
+vector <int> ExactTreeEasy::getTree(int n, int m) {
+	vector<int> vec;
+	if(m==n-1){
+		for(int i = 1 ; i<n ; i++){
+			vec.push_back(0);
+			vec.push_back(i);
+		}
+		return vec;
+	}
+	int M=m;
+	vec.push_back(0);
+	vec.push_back(1);
+	vec.push_back(0);
+	vec.push_back(2);
+	if(m==1 && n==3)
+	return vec;
+	m--;
+	int current = 2;
+	while(current+m<n-1){
+		vec.push_back(current);
+		vec.push_back(current+1);
+		current++;
+	}
+	for(int i=current+1; i<n ; i++){
+		vec.push_back(current);
+		vec.push_back(i);
+	}
+	return vec;
+	
+}
+
+
+double test0() {
+	int p0 = 7;
+	int p1 = 6;
+	ExactTreeEasy * obj = new ExactTreeEasy();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getTree(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {0, 1, 1, 2, 2, 3 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 7;
+	int p1 = 5;
+	ExactTreeEasy * obj = new ExactTreeEasy();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getTree(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {0, 1, 1, 2, 1, 3 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 7;
+	int p1 = 4;
+	ExactTreeEasy * obj = new ExactTreeEasy();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getTree(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {0, 1, 1, 2 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 7;
+	int p1 = 3;
+	ExactTreeEasy * obj = new ExactTreeEasy();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getTree(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {0, 1, 1, 2, 1, 3, 3, 4 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int p0 = 7;
+	int p1 = 2;
+	ExactTreeEasy * obj = new ExactTreeEasy();
+	clock_t start = clock();
+	vector <int> my_answer = obj->getTree(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int t2[] = {0, 1, 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 7, 1, 8, 1, 9 };
+	vector <int> p2(t2, t2+sizeof(t2)/sizeof(int));
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t{ ";
+	if (p2.size() > 0) {
+		cout <<p2[0];
+		for (int i=1; i<p2.size(); i++)
+			cout <<", " <<p2[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	cout <<endl <<"Your answer: " <<endl;
+	cout <<"\t{ ";
+	if (my_answer.size() > 0) {
+		cout <<my_answer[0];
+		for (int i=1; i<my_answer.size(); i++)
+			cout <<", " <<my_answer[i];
+		cout <<" }" <<endl;
+	}
+	else
+		cout <<"}" <<endl;
+	if (my_answer != p2) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
