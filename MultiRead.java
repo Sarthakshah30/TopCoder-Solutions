6a60412b4dda1ec66c1e4e696c96351a44846a20
Sarthak Shah <Sarthakshah30@rocketmail.com>
2018-02-23 22:49:48 +0530

    Add files via upload

---
+++ MultiRead.java
@@ -0,0 +1,111 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+import java.awt.geom.*;
+
+public class MultiRead
+{
+	public int minCycles(String trace, int procs)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		long time;
+		int answer;
+		boolean errors = false;
+		int desiredAnswer;
+		
+		
+		time = System.currentTimeMillis();
+		answer = new MultiRead().minCycles("RWWRRR", 3);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 4;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new MultiRead().minCycles("RWWRRRR", 3);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 5;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new MultiRead().minCycles("WWWWW", 5);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 5;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new MultiRead().minCycles("RRRRRRRRRR", 4);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 3;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new MultiRead().minCycles("RWRRWWRWRWRRRWWRRRRWRRWRRWRRRRRRRRRWRWRWRRRRWRRRRR", 4);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 30;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		
+		
+		if (errors)
+			System.out.println("Some of the test cases had errors :-(");
+		else
+			System.out.println("You're a stud (at least on the test data)! :-D ");
+	}
+
+}
+//Powered by [KawigiEdit] 2.0!
