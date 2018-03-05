1aa49671386b6b7a811a3845d14248164f1d0d40
Sarthak Shah <Sarthakshah30@rocketmail.com>
2018-03-05 22:58:38 +0530

    Add files via upload

---
+++ BiggestRectangleEasy.java
@@ -0,0 +1,115 @@
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import java.math.*;
+import java.awt.geom.*;
+
+public class BiggestRectangleEasy
+{
+	public int findArea(int N)
+	{
+		int maxArea=0;
+		for(int i = 1 ; 2*i <=N-2 ; i++){
+			maxArea=Math.max(maxArea,(i)*((N-2*i)/2));
+		}
+		return maxArea;
+	}
+	
+	public static void main(String[] args)
+	{
+		long time;
+		int answer;
+		boolean errors = false;
+		int desiredAnswer;
+		
+		
+		time = System.currentTimeMillis();
+		answer = new BiggestRectangleEasy().findArea(11);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 6;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new BiggestRectangleEasy().findArea(5);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 1;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new BiggestRectangleEasy().findArea(64);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 256;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new BiggestRectangleEasy().findArea(753);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 35344;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		time = System.currentTimeMillis();
+		answer = new BiggestRectangleEasy().findArea(7254);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
+		desiredAnswer = 3288782;
+		System.out.println("Your answer:");
+		System.out.println("\t" + answer);
+		System.out.println("Desired answer:");
+		System.out.println("\t" + desiredAnswer);
+		if (answer != desiredAnswer)
+		{
+			errors = true;
+			System.out.println("DOESN'T MATCH!!!!");
+		}
+		else
+			System.out.println("Match :-)");
+		System.out.println();
+		
+		
+		if (errors)
+			System.out.println("Some of the test cases had errors :-(");
+		else
+			System.out.println("You're a stud (at least on the test data)! :-D ");
+	}
+
+}
+//Powered by [KawigiEdit] 2.0!
